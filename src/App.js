d0ec18e98d5d6ca91168ce484228fe9de669a0c7
huanmingchang <huanming1988@gmail.com>
2022-08-27 10:35:15 +0800

    feat: 新增 todos component
    因應新需求做調整：
      新增 todos component
    
    調整項目：
    1. 新增 Todos.js component 以及新增 JSX 結構
    2. 新增 Todo list tabs
    3. 新增 Todo list statistics 項目

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css'
 import Nav from './components/Nav'
 import Input from './components/Input'
+import Todos from './components/Todos'
 
 function App() {
   return (
@@ -10,6 +11,30 @@ function App() {
         <div className='conatiner todoListPage vhContainer'>
           <div className='todoList_Content'>
             <Input />
+            <div className='todoList_list'>
+              <ul className='todoList_tab'>
+                <li>
+                  <a href='#' className='active'>
+                    全部
+                  </a>
+                </li>
+                <li>
+                  <a href='#'>待完成</a>
+                </li>
+                <li>
+                  <a href='#'>已完成</a>
+                </li>
+              </ul>
+              <div className='todoList_items'>
+                <ul className='todoList_item'>
+                  <Todos />
+                </ul>
+                <div className='todoList_statistics'>
+                  <p> 5 個已完成項目</p>
+                  <a href='#'>清除已完成項目</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
