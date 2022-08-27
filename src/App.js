af3ddef83f08528cd2c6bce5d657c5d3f82e94da
huanmingchang <huanming1988@gmail.com>
2022-08-27 10:15:15 +0800

    feat: 新增 nav component
    因應新需求做調整：
      新增 nav component
    
    調整項目：
    1. 新增 Nav.js component 以及新增 JSX 結構
    2. 調整 nav background image 路徑
    3. 在 App.js 中引入 Nav.js 元件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './styles/App.css'
+import Nav from './components/Nav'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <div id='todoListPage' className='bg-half'>
+        <Nav />
+      </div>
+    </div>
+  )
 }
 
 export default App
