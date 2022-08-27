0cddcc0218d8c3a799cf285e65fe33a013b1e037
huanmingchang <huanming1988@gmail.com>
2022-08-27 10:23:42 +0800

    feat: 新增 Input component
    因應新需求做調整：
      新增 Input component
    
    調整項目：
    1. 新增 Input.js component 以及新增 JSX 結構
    2. 在 index.html 中引入 fontawesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './styles/App.css'
 import Nav from './components/Nav'
+import Input from './components/Input'
 
 function App() {
   return (
     <div className='App'>
       <div id='todoListPage' className='bg-half'>
         <Nav />
+        <div className='conatiner todoListPage vhContainer'>
+          <div className='todoList_Content'>
+            <Input />
+          </div>
+        </div>
       </div>
     </div>
   )
